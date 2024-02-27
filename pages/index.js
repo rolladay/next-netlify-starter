d1dd3fdfb28ee671fcb8cdd85370c236287843e4
rolladay <122243679+rolladay@users.noreply.github.com>
2024-02-27 23:11:38 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>rolladay</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Rolladay Home" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Sorry, I`m preparing service. please wait.. big a moment</code>
         </p>
       </main>
 
